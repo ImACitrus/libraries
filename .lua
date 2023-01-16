191f0af151bf7b02e38a0c221293590d3a5a6de2
ImACitrus <109486300+ImACitrus@users.noreply.github.com>
2023-01-16 23:54:09 +0100

    Update .lua

--- .lua
+++ .lua
@@ -20,49 +20,53 @@ function module:Window(config)
     local Player = Players.LocalPlayer
     local PlayerGui = Player:WaitForChild("PlayerGui")
 
-    local Gui = Instance.new("ScreenGui", PlayerGui)
+    local Gui = PlayerGui:FindFirstChild("wl") or Instance.new("ScreenGui", PlayerGui)
     local body = Instance.new("Frame", Gui)
 
     local key = Instance.new("TextBox", body)
     local trigger = Instance.new("TextButton", body)
 
-    Gui.Name = "wl"
+    if not Gui.Name == "wl" then
 
-    body.Name = "body"
-    key.Name = "key"
-    trigger.Name = "callback"
+        Gui.Name = "wl"
+
+        body.Name = "body"
+        key.Name = "key"
+        trigger.Name = "callback"
+        
+        body.Active = true
+        body.Draggable = true
+        body.AnchorPoint = Vector2.new(0.5, 0.5)
+        body.Position = UDim2.fromScale(0.5, 0.5)
+        body.Size = UDim2.fromOffset(400, 105)
+
+        body.BorderSizePixel = 0
+
+        key.Size = UDim2.fromOffset(378, 35)
+        key.Position = UDim2.fromScale(0.027, 0.146)
+        
+        key.Text = ""
+        key.PlaceholderText = "Enter key"
+        key.BorderSizePixel = 1
+        
+        trigger.Text = "Submit"
+        trigger.Size = UDim2.fromOffset(378, 41)
+        trigger.Position = UDim2.fromScale(0.027, 0.539)
+
+        trigger.BorderSizePixel = 0
+        trigger.FontSize = Enum.FontSize.Size18
+
+        body.BackgroundColor3 = config and config.Properties and config.Properties.body or Color3.fromRGB(25, 25, 25)
+
+        key.TextColor3 = config and config.Properties and config.Properties.key.textcolor or Color3.fromRGB(178, 178, 178)
+        key.BackgroundColor3 = config and config.Properties and config.Properties.key.backgroundcolor or Color3.fromRGB(20, 20, 20)
+        key.BorderColor3 = config and config.Properties and config.Properties.key.outlinecolor or Color3.fromRGB(15, 15, 15)
+
+        trigger.TextColor3 = config and config.Properties and config.Properties.button.textcolor or Color3.fromRGB(229, 229, 229)
+        trigger.BackgroundColor3 = config and config.Properties and config.Properties.button.backgroundcolor or Color3.fromRGB(35, 35, 35)
+
+    end
     
-    body.Active = true
-    body.Draggable = true
-    body.AnchorPoint = Vector2.new(0.5, 0.5)
-    body.Position = UDim2.fromScale(0.5, 0.5)
-    body.Size = UDim2.fromOffset(400, 105)
-
-    body.BorderSizePixel = 0
-
-    key.Size = UDim2.fromOffset(378, 35)
-    key.Position = UDim2.fromScale(0.027, 0.146)
-    
-    key.Text = ""
-    key.PlaceholderText = "Enter key"
-    key.BorderSizePixel = 1
-    
-    trigger.Text = "Submit"
-    trigger.Size = UDim2.fromOffset(378, 41)
-    trigger.Position = UDim2.fromScale(0.027, 0.539)
-
-    trigger.BorderSizePixel = 0
-    trigger.FontSize = Enum.FontSize.Size18
-
-    body.BackgroundColor3 = config and config.Properties and config.Properties.body or Color3.fromRGB(25, 25, 25)
-
-    key.TextColor3 = config and config.Properties and config.Properties.key.textcolor or Color3.fromRGB(178, 178, 178)
-    key.BackgroundColor3 = config and config.Properties and config.Properties.key.backgroundcolor or Color3.fromRGB(20, 20, 20)
-    key.BorderColor3 = config and config.Properties and config.Properties.key.outlinecolor or Color3.fromRGB(15, 15, 15)
-
-    trigger.TextColor3 = config and config.Properties and config.Properties.button.textcolor or Color3.fromRGB(229, 229, 229)
-    trigger.BackgroundColor3 = config and config.Properties and config.Properties.button.backgroundcolor or Color3.fromRGB(35, 35, 35)
-
     local TweenService = game:GetService("TweenService")
     local self = setmetatable({}, module)
     
@@ -83,11 +87,6 @@ function module:Window(config)
             })
             tween:Play()
         end
-    end
-    
-    function self:LoadUrl(url)
-       if not url or type(url)~="string" then return end;
-       loadstring(game:HttpGet(url))() 
     end
     
     return self
@@ -138,21 +137,17 @@ local SETTINGS = module.SETTINGS
 
 SETTINGS.closeAfterX = 0
 
+
 keys = {
     {
-        Id = 4112844200, -- AgentRBLX23
+        Id = 4112844200,
         Key = "0DAA43C3-F1FC-4D36-B415-0C43E3C1EF06",
-        Subscription = true
+        Subscription = false
     },
     {
-        Id = 277225187, -- Underscore_
-        Key = "6E5E6930-50A8-46FE-97AD-C2AE9CCB4852",
-        Subscription = true
-    },
-    {
-        Id = 1361218435, -- HapsiGame1
+        Id = 1361218435,
         Key = "3808FBF8-0484-464A-98E7-12E0D28972E4",
-        Subscription = true
+        Subscription = false
     },
 }
 
